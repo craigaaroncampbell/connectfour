c5af8bec107e7045fa78a0ccc2eadcba1d88b84e
Craig Campbell <craigaaroncampbell@gmail.com>
2016-02-26 22:31:55 -0800

    stacking works on column 0

--- setup-board.js
+++ setup-board.js
@@ -31,29 +31,46 @@ $('.circle').on('click', function(){
   //if a column is not full
   if (boardArr[0][colNum] === '-') {
     var k = colNum;
+   function addToBoardArr(){
+      if (boardArr[1][colNum] === 'R') boardArr[0][colNum] = 'R';
+      if (boardArr[2][colNum] === 'R') boardArr[1][colNum] = 'R';
+      if (boardArr[3][colNum] === 'R') boardArr[2][colNum] = 'R';
+      if (boardArr[4][colNum] === 'R') boardArr[3][colNum] = 'R';
+      if (boardArr[5][colNum] === 'R') boardArr[4][colNum] = 'R';
+      if (boardArr[5][colNum] === '-') boardArr[5][colNum] = 'R';
+    }
     function nextCircle(){
       setTimeout(function(){
         $('#circle' + k).css('background-color', 'red');
-
-        //if the spot directly below is blank
         if (k > 6) {
-          // console.log(colNum, rowNum)
-          // console.log(boardArr[colNum][rowNum])
           $('#circle' + (k - 7)).removeAttr('style');
         }
 
         k += 7;
 
-        if (k < 42) {
-          nextCircle();
+        if (k - 7 === 0 && $('#circle' + k).prop('style')[0] === "background-color" ){
+          console.log("shit")
+          $('#circle' + k-7).css('background-color', 'red');
+          addToBoardArr();
         }
         else {
-          if (boardArr[1][colNum] === 'R') boardArr[0][colNum] = 'R';
-          if (boardArr[2][colNum] === 'R') boardArr[1][colNum] = 'R';
-          if (boardArr[3][colNum] === 'R') boardArr[2][colNum] = 'R';
-          if (boardArr[4][colNum] === 'R') boardArr[3][colNum] = 'R';
-          if (boardArr[5][colNum] === 'R') boardArr[4][colNum] = 'R';
-          if (boardArr[5][colNum] === '-') boardArr[5][colNum] = 'R';
+          var nextFilled;
+          if (  k < 42  ) {
+            if (k + 7 < 42 ) {
+              if ($('#circle' + (k + 7)).prop('style')[0] === "background-color" ) {
+                nextFilled = true;
+                $('#circle' + k).css('background-color', 'red');
+                $('#circle' + (k - 7)).removeAttr('style');
+                addToBoardArr();
+              }
+            }
+            if (!nextFilled) {
+              nextCircle();
+            }
+          }
+          else {
+            addToBoardArr();
+          }
         }
       }, 250);
     }
