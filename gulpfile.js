b8c760fef1bfa0c44bc9e4b67d780e10dd811b2e
Craig Campbell <craigaaroncampbell@gmail.com>
2016-03-05 20:46:23 -0800

    some change

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,16 @@ var maps = require('gulp-sourcemaps');
 var minifyCss = require('gulp-minify-css');
 
 gulp.task('sass:dev', function() {
-  gulp.src('./*.scss')
+  gulp.src('./*.scss') // pick your OWN path here
   .pipe(maps.init())
   .pipe(sass().on('error', sass.logError))
   .pipe(minifyCss())
-  .pipe(maps.write('./'))
+  .pipe(maps.write('./')) // NOT ./build!
   .pipe(gulp.dest('./build'));
 });
 
 gulp.task('default', ['sass:dev']);
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./*.scss', ['sass:dev']);
+});
